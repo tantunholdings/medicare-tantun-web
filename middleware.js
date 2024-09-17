69ac3bb63852117628252d907d9661a32714fff3
sahiru <sahiruwrcg@gmail.com>
2024-09-17 23:59:45 +0530

    Enhance middleware for token validation and redirect logic

--- middleware.js
+++ middleware.js
@@ -1,15 +1,35 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(req) {
-  const token = req.cookies.get('token');
-    // console.log(token)
+export async function middleware(req) {
+  const token = req.cookies.get('token'); // Get token from cookies
   const protectedRoutes = ['/admin', '/blog/new'];
+
   if (!token && protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  const response = NextResponse.next()
-  response.cookies.set('token', 'Your secret token')
+  if (token  && protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
+    try {
+
+      const validateUrl = new URL('/auth/validate', process.env.NEXT_PUBLIC_FASTAPI_URL);
+      const response = await fetch(validateUrl, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token?.value}`,
+        },
+      });
+
+      if (!response.ok) {
+        return NextResponse.redirect(new URL('/login', req.url));
+      }
+
+
+      return NextResponse.next();
+    } catch (error) {
+      console.error('Error validating token:', error);
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+  }
 
   return NextResponse.next();
 }
