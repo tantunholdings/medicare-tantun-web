d4fa67261a225290717df36df80773752a5b9185
sahiru <sahiruwrcg@gmail.com>
2024-10-08 21:37:55 +0530

    Update page title in InsuranceAdvisor component

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function InsuranceAdvisor() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Insurance Advisor</title>
+        <title>Insurance Advisor-</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <link
           rel="stylesheet"
