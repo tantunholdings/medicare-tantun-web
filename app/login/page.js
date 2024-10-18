df11a415ca8c84b00af8db0a16c3be3bc0982e70
sahiru <sahiruwrcg@gmail.com>
2024-10-19 04:16:30 +0530

    Refactor fetch URL in FAQEditorPage to use environment variable

--- app/login/page.js
+++ app/login/page.js
@@ -5,7 +5,7 @@ import LoginForm from '@/components/LoginForm';
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-semibold mb-4">Login...</h1>
+      <h1 className="text-2xl font-semibold mb-4">Login</h1>
       <LoginForm />
     </div>
   );
