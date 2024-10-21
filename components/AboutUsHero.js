d8193fa466060f9058ccbd7e788f00205ade8399
sahiru <sahiruwrcg@gmail.com>
2024-10-22 01:50:37 +0530

    Refactor AboutUsHero component for better responsiveness and update font sizes

--- components/AboutUsHero.js
+++ components/AboutUsHero.js
@@ -3,9 +3,18 @@ import React from "react";
 const AboutUsHero = () => {
   return (
     <div className="bg-white flex items-center justify-center">
-      <div className="mx-auto px-20 bg-white rounded-lg flex items-center m-4">
+      <div className="mx-auto px-6 sm:px-20 bg-white rounded-lg flex flex-col sm:flex-row items-center m-4">
+        {/* Image Section */}
+        <div className="w-full sm:w-1/2 flex justify-center mb-4 sm:mb-0">
+          <img
+            src="/assets/hero-image.png"
+            alt="Illustration of people comparing healthcare plans under an umbrella with a heart and shield symbol"
+            className="object-contain w-4/5 h-4/5" // Scaled down to 80% size
+          />
+        </div>
+
         {/* Text Section */}
-        <div className="w-1/2">
+        <div className="w-full sm:w-1/2 text-justify sm:text-left">
           <h1 className="text-4xl font-bold text-primaryBlack mb-4">
             Who is Tantun?
           </h1>
@@ -18,15 +27,6 @@ const AboutUsHero = () => {
             personalized, unbiased recommendations over the phone from licensed
             agents. The service is free with no obligation to enroll.
           </p>
-        </div>
-
-        {/* Image Section */}
-        <div className="w-1/2 flex justify-center">
-          <img
-            src="/assets/hero-image.png"
-            alt="Illustration of people comparing healthcare plans under an umbrella with a heart and shield symbol"
-            className="object-contain w-4/5 h-4/5" // Scaled down to 80% size
-          />
         </div>
       </div>
     </div>
