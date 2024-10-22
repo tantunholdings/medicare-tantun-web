07f9338a1ff7716c450159f9005a3d39c8aa2d05
sahiru <sahiruwrcg@gmail.com>
2024-10-23 02:33:17 +0530

    Test CI

--- components/TawkMessengerReact.js
+++ components/TawkMessengerReact.js
@@ -11,7 +11,7 @@ const TawkMessengerReact = () => {
       (function(){
       var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
       s1.async=true;
-      s1.src='https://embed.tawk.to/67138b002480f5b4f58ff64a/1iai5dup9';
+      s1.src='https://tawk.to/chat/67138b002480f5b4f58ff64a/1iai5dup9';
       s1.charset='UTF-8';
       s1.setAttribute('crossorigin','*');
       s0.parentNode.insertBefore(s1,s0);
