5ed3ecccd4fede95d82815b4af4a556823bc1a25
sahiru <sahiruwrcg@gmail.com>
2024-11-19 17:07:56 +0530

    Refactor EditorConvertToHTML component to use functional component with hooks

--- app/admin/blog/new/EditorConvertToHTML.js
+++ app/admin/blog/new/EditorConvertToHTML.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { EditorState, convertToRaw, ContentState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
@@ -8,67 +8,38 @@ import htmlToDraft from "html-to-draftjs";
 
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-class EditorConvertToHTML extends Component {
-  constructor(props) {
-    super(props);
+const EditorConvertToHTML = ({ content, setContent }) => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-    let editorState = EditorState.createEmpty();
-    // If content is passed, convert it to editor state
-    if (props.content) {
-      const blocksFromHtml = htmlToDraft(props.content);
+  useEffect(() => {
+    if (content && !editorState.getCurrentContent().hasText()) {
+      const blocksFromHtml = htmlToDraft(content);
       const { contentBlocks, entityMap } = blocksFromHtml;
       const contentState = ContentState.createFromBlockArray(
         contentBlocks,
         entityMap
       );
-      editorState = EditorState.createWithContent(contentState);
+      const newEditorState = EditorState.createWithContent(contentState);
+      setEditorState(newEditorState);
     }
+  }, [content]);
 
-    this.state = {
-      editorState,
-    };
-  }
-
-  onEditorStateChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
-
-    this.props.setContent(
-      draftToHtml(convertToRaw(editorState.getCurrentContent()))
-    );
+  const handleEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+    setContent(draftToHtml(convertToRaw(newEditorState.getCurrentContent())));
   };
 
-  // componentDidUpdate(prevProps) {
-  //   if (this.props.content !== prevProps.content) {
-  //     const blocksFromHtml = htmlToDraft(this.props.content);
-  //     const { contentBlocks, entityMap } = blocksFromHtml;
-  //     const contentState = ContentState.createFromBlockArray(
-  //       contentBlocks,
-  //       entityMap
-  //     );
-  //     const newEditorState = EditorState.createWithContent(contentState);
-
-  //     // Ensure cursor position is maintained
-  //     const updatedEditorState = EditorState.moveFocusToEnd(newEditorState);
-  //     this.setState({ editorState: updatedEditorState });
-  //   }
-  // }
-
-  render() {
-    const { editorState } = this.state;
-    return (
-      <div className="border border-gray-300 p-2 rounded-md">
-        <Editor
-          editorState={editorState}
-          wrapperClassName="demo-wrapper"
-          editorClassName="demo-editor min-h-[200px] border border-gray-300 p-2 rounded"
-          onEditorStateChange={this.onEditorStateChange}
-          toolbarClassName="bg-gray-50 border border-gray-300 rounded"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="border border-gray-300 p-2 rounded-md">
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor min-h-[200px] border border-gray-300 p-2 rounded"
+        onEditorStateChange={handleEditorStateChange}
+        toolbarClassName="bg-gray-50 border border-gray-300 rounded"
+      />
+    </div>
+  );
+};
 
 export default EditorConvertToHTML;
